de988f0cea7ae996504ad4912684885120e9fe27
Alyona <fedalena417@gmail.com>
2026-08-12 18:26:29 +0300

    Четвертый урок

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
+import React from "react";
 import Index from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
-const arr = [
-    { title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-        price: "12 999",
-        imgUrl: '/img/sneakers/1.jpg'},
-
-    { title: 'Мужские Кроссовки Nike Air Max 270',
-        price: "15 600",
-        imgUrl: '/img/sneakers/2.jpg' },
-
-    { title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-        price: "8 499",
-        imgUrl: '/img/sneakers/3.jpg' },
-
-    { title: 'Кроссовки Puma X Aka Boku Future Rider',
-        price: "8 999",
-        imgUrl: '/img/sneakers/4.jpg' }
-];
-
 function App() {
-  return (
-      <div className="wrapper clear">
-          <Drawer />
-          <Header />
-          <div className="content p-40">
-              <div className="d-flex align-center justify-between mb-40">
-                  <h1>Все кроссовки</h1>
-                  <div className="search-block d-flex">
-                      <img src="/img/search.svg" alt="Search"/>
-                      <input placeholder="Поиск..."/>
-                  </div>
-              </div>
+    const [items, setItems] = React.useState([]);
+    const [cartItems, setCartItems] = React.useState([]);
+    const [cartOpened, setCartOpened] = React.useState(false);
 
-              <div className="d-flex">
-                  {arr.map((obj) => (
-                      <Index
-                          title = {obj.title}
-                          price = {obj.price}
-                          imageUrl = {obj.imgUrl}
-                          onClick = {() => console.log(obj)}
-                      />
-                  ))}
-              </div>
-          </div>
-      </div>
-  );
+    React.useEffect(() => {
+        fetch('https://6a7c6fe0a008c10e4cbf6a30.mockapi.io/items')
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                setItems(json)
+            });
+    }, [])
+
+    const onAddToCart = (obj) => {
+        setCartItems([...cartItems, obj]);
+    };
+
+    return (
+        <div className="wrapper clear">
+            {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
+            <Header onClickCart={() => setCartOpened(true)} />
+            <div className="content p-40">
+                <div className="d-flex align-center justify-between mb-40">
+                    <h1>Все кроссовки</h1>
+                    <div className="search-block d-flex">
+                        <img src="/img/search.svg" alt="Search"/>
+                        <input placeholder="Поиск..."/>
+                    </div>
+                </div>
+
+                <div className="d-flex flex-wrap justify-around">
+                    {items.map((item) => (
+                        <Index
+                            title = {item.title}
+                            price = {item.price}
+                            imageUrl = {item.imageUrl}
+                            onFavorite = {() => console.log('Добавили закладки')}
+                            onPlus = {(obj) => onAddToCart(obj)}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
